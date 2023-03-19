b255b3cae45c5fea953d7b62eb8717bf5e117809
porqueSammy <42855916+porquesammy@users.noreply.github.com>
2023-03-18 21:31:45 -0700

    add ability to draw and double click for erase.

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ const gridLineState = document.querySelector(
   '.switch.lines input[type="checkbox"]'
 );
 const slider = document.querySelector("#slider");
-let allDivs;
-const gridColorPicker = document.querySelector('#gridColorPicker');
+const gridColorPicker = document.querySelector("#gridColorPicker");
 let sliderValue = document.querySelector("#rangeValue").innerText;
 const defaultGrid = Math.pow(16, 2);
 let selectedGrid = defaultGrid;
 
+gridColorPicker.onchange = gridBackgroundColor;
+gridLineState.onchange = gridLines;
 
 //grid slider power of 2 for even grids
 slider.onchange = function () {
@@ -22,7 +23,7 @@ slider.onchange = function () {
 function createGrid() {
   clearGridDivs();
   [...document.styleSheets[0].cssRules].find(
-    (x) => x.selectorText === "#container"
+    (i) => i.selectorText === "#container"
   ).style.cssText =
     "grid-template-columns: repeat(" +
     `${Math.sqrt(selectedGrid)}` +
@@ -35,11 +36,9 @@ function createGrid() {
   }
   gridLines();
   gridBackgroundColor();
+  divEventListeners();
 }
-
-/* temp call to debug */
 createGrid();
-/* temp call to debug */
 
 function clearGridDivs() {
   let allDivs = document.querySelectorAll("#container>*");
@@ -49,30 +48,61 @@ function clearGridDivs() {
   }
 }
 
-gridLineState.onchange = gridLines;
-
 function gridLines() {
-  allDivs = document.querySelectorAll(".container>*");
+  let allDivs = document.querySelectorAll(".container>*");
   if (gridLineState.checked) {
-    allDivs.forEach((x) => {
-      x.style.border = "rgb(156, 156, 156) 0.1px solid";
+    allDivs.forEach((i) => {
+      i.style.border = "rgb(156, 156, 156) 0.1px solid";
     });
   } else {
-    allDivs.forEach((x) => (x.style.border = "none"));
+    allDivs.forEach((i) => (i.style.border = "none"));
   }
 }
 
-
-
-gridColorPicker.onchange = gridBackgroundColor;
-
-function gridBackgroundColor() {
-    let currentColor = gridColorPicker.value;
-    allDivs = document.querySelectorAll(".container>*");
-    allDivs.forEach((x)=>{
-        x.style.backgroundColor = `${currentColor}`;
-    });
+// helper for rainbow
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
 }
 
+function gridBackgroundColor() {
+  let currentColor = gridColorPicker.value;
+  let allDivs = document.querySelectorAll(".container>*");
+  allDivs.forEach((i) => {
+    i.style.backgroundColor = `${currentColor}`;
+  });
+}
 
+// clear button
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", createGrid);
 
+//add event listeners to every div
+function divEventListeners() {
+  let allDivs = document.querySelectorAll(".container>*");
+  allDivs.forEach((i) => {
+    i.addEventListener("mouseover", draw);
+    i.addEventListener("mousedown", draw);
+    i.addEventListener("dblclick", erase);
+    //fix bug where mouseup happens outside of divs and keeps drawing
+    window.addEventListener("mouseup", stopDraw);
+  });
+}
+
+let mouseDown = false;
+function draw(e) {
+  if (e.type === "mousedown") {
+    e.preventDefault();
+    mouseDown = true;
+    e.toElement.style.backgroundColor = "red";
+  } else if (e.type === "mouseover" && mouseDown) {
+    e.toElement.style.backgroundColor = "red";
+  }
+}
+
+function stopDraw() {
+  mouseDown = false;
+}
+
+function erase(e) {
+  e.toElement.style.backgroundColor = `${gridColorPicker.value}`;
+}
